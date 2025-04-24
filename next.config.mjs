910b4950501e65450660dc38359d1ee50cd68f01
Bhanu Pratap Jha <122882101+bhanu-pratap-jha@users.noreply.github.com>
2025-04-24 23:01:37 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    PGUSER: process.env.PGUSER,
+    PGHOST: process.env.PGHOST,
+    PGDATABASE: process.env.PGDATABASE,
+    PGPASSWORD: process.env.PGPASSWORD,
+    PGPORT: process.env.PGPORT,
+  },
+};
 
 export default nextConfig;
